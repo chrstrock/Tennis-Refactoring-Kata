03a6f89dda012d2745dcc3269cc8949b82bf0fb0
David Ferrell <chrstrock@gmail.com>
2026-09-10 11:23:12 -0400

    r extract methods out of getScore to simplify.

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -2,8 +2,10 @@ public class TennisGame1 implements TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
+    private StringBuilder score;
 
     public TennisGame1() {
+        score = new StringBuilder();
     }
 
     public void wonPoint(String playerName) {
@@ -14,32 +16,44 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        StringBuilder score = new StringBuilder();
         if (m_score1==m_score2)
         {
-            if(m_score1 < 3){
-                determineScore(score, m_score1);
-                score.append("-All");
-            }
-            else {
-                score.append("Deuce");
-            }
+            tiedScore(score);
         }
         else if (m_score1>=4 || m_score2>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score = new StringBuilder("Advantage player1");
-            else if (minusResult ==-1) score = new StringBuilder("Advantage player2");
-            else if (minusResult>=2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
+            score = scoreDeuce();
         }
         else
         {
-            determineScore(score, m_score1);
-            score.append("-");
-            determineScore(score, m_score2);
+            displayScore(score);
         }
         return score.toString();
+    }
+
+    private void displayScore(StringBuilder score) {
+        determineScore(score, m_score1);
+        score.append("-");
+        determineScore(score, m_score2);
+    }
+
+    private StringBuilder scoreDeuce() {
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) score = new StringBuilder("Advantage player1");
+        else if (minusResult ==-1) score = new StringBuilder("Advantage player2");
+        else if (minusResult>=2) score = new StringBuilder("Win for player1");
+        else score = new StringBuilder("Win for player2");
+        return score;
+    }
+
+    private void tiedScore(StringBuilder score) {
+        if(m_score1 < 3){
+            determineScore(score, m_score1);
+            score.append("-All");
+        }
+        else {
+            score.append("Deuce");
+        }
     }
 
     private void determineScore(StringBuilder score, int mScore1) {
